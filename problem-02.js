c0a884f4f468025f8bb80bc20c498ada65db3d18
mzamanasif <explorerasif404@gmail.com>
2026-02-22 13:34:24 +0600

    problem 02 solved

---
+++ problem-02.js
@@ -0,0 +1,13 @@
+var money = 99999;
+
+if(money > 0 && money <= 10^9){
+    if(money >= 25000){
+        console.log('Laptop');
+    }
+    else if(money >= 10000){
+        console.log('Cycle');
+    }
+    else{
+        console.log('Chocolate');
+    }
+}
